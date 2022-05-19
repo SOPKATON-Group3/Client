e264575e310dccc314088600ebac50d57eb20a36
Hennization <shy6295@ewhain.net>
2022-05-19 14:49:59 +0900

    fix: 절대경로 수정

--- src/component/common/Router.jsx
+++ src/component/common/Router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../../pages/home/Home";
-import MyPage from "../../pages/myPage/MyPage";
+import Home from "pages/Home";
+import MyPage from "pages/MyPage";
 function Router() {
   return (
     <>
