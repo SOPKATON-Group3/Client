21958e881f71308c4a5e4f5e08345c34e1722dfd
Hennization <shy6295@ewhain.net>
2022-05-19 11:50:41 +0900

    init: 파일구조 세팅

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Layout from "./components/common/Layout";
+import Router from "./components/common/Router";
+function App() {
+  return (
+    <Layout>
+      <Router />
+    </Layout>
+  );
+}
+
+export default App;
