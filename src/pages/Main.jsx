353bd27bccaa4c618b94e4147e6d9b79e7e8d1f6
leeseooo <leeseoooo@naver.com>
2022-05-22 03:57:01 +0900

    chore: 배경화면 url 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { ReactComponent as Logo } from "component/common/assets/icon/logo.svg";
-import mainBackground from "component/common/assets/images/mainBackground.svg";
 import wireBackgroundImg, {
   ReactComponent as Wire,
 } from "component/common/assets/images/wire.svg";
@@ -8,7 +7,7 @@ import UnLockContainer from "component/main/unLockContainer";
 
 function Main() {
   return (
-    <StyledRoot url={mainBackground}>
+    <StyledRoot>
       <StyledLogoWrapper>
         <Logo />
       </StyledLogoWrapper>
@@ -27,7 +26,6 @@ export default Main;
 const StyledRoot = styled.div`
   display: flex;
   flex-direction: column;
-  background-image: ${(props) => `url(${props.url})`};
   position: relative;
   width: 100vw;
   height: 100vh;
