f1fa1e19af0030ceb0020881f2229da003d26862
HyeJun <solar3070@naver.com>
2022-05-22 07:44:58 +0900

    css: 폰트, cursor pointer 변경

--- src/component/main/Header.jsx
+++ src/component/main/Header.jsx
@@ -75,6 +75,7 @@ const StyledButtonWrapper = styled.div`
     background-color: transparent;
     color: white;
     color: ${({ isDayTime }) => isDayTime && `#000`};
+    cursor: pointer;
   }
 `;
 
