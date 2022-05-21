966359bd4093803c2db045ebffd27d35a7b23b51
Hennization <shy6295@ewhain.net>
2022-05-21 23:02:56 +0900

    feat: 이미지 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+import { ReactComponent as Logo } from "component/common/assets/icon/logo.svg";
+import mainBackgroundImg from "component/common/assets/images/mainBackgroundImg.svg";
+import wire from "component/common/assets/images/wire.svg";
+
 function Main() {
-  return <div>Main</div>;
+  return (
+    <StyledRoot>
+      <Logo />
+      <img src={wire} alt="" />
+    </StyledRoot>
+  );
 }
 
 export default Main;
+
+const StyledRoot = styled.div`
+  background-image: ${mainBackgroundImg};
+`;
