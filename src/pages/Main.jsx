54917614e8b72c8a3f5a3fc28034b8d04cc9e77c
Hennization <shy6295@ewhain.net>
2022-05-22 01:20:19 +0900

    feat: 자물쇠 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 import { ReactComponent as Logo } from "component/common/assets/icon/logo.svg";
 import mainBackgroundImg from "component/common/assets/images/mainBackgroundImg.svg";
-import wire from "component/common/assets/images/wire.svg";
+import wireBackgroundImg, {
+  ReactComponent as Wire,
+} from "component/common/assets/images/wire.svg";
+import UnLockContainer from "component/main/unLockContainer";
 
 function Main() {
   return (
-    <StyledRoot>
-      <Logo />
-      <img src={wire} alt="" />
+    <StyledRoot url={mainBackgroundImg}>
+      <StyledLogoWrapper>
+        <Logo />
+      </StyledLogoWrapper>
+      <UnLockContainer />
+      <StyledWireBackground url={wireBackgroundImg} />
     </StyledRoot>
   );
 }
@@ -15,5 +21,24 @@ function Main() {
 export default Main;
 
 const StyledRoot = styled.div`
-  background-image: ${mainBackgroundImg};
+  display: flex;
+  flex-direction: column;
+  background-image: ${(props) => `url(${props.url})`};
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const StyledLogoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 4.4rem;
+`;
+
+const StyledWireBackground = styled.div`
+  position: absolute;
+  background-image: ${(props) => `url(${props.url})`};
+  width: 100%;
+  height: 58%;
+  bottom: 0;
 `;
