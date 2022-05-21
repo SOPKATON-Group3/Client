5f3708ba726b26367f18f3b54f2d48a6d8011347
leeseooo <leeseoooo@naver.com>
2022-05-22 07:14:07 +0900

    chore: css 수정

--- src/component/MessageFeedContainer/index.jsx
+++ src/component/MessageFeedContainer/index.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 import Messages from "./Messages";
+import { default as closeButton } from "component/common/assets/icon/close.svg";
+import { useNavigate } from "react-router-dom";
 
 function MessageFeedContainer() {
+  const navigate = useNavigate();
   return (
     <StyledRoot>
       <StyledWrapper />
-      <StyledHeader>누군가 열어본 즐거움</StyledHeader>
-
+      <StyledHeaderWrapper>
+        <StyledHeader>누군가 열어본 즐거움</StyledHeader>
+        <button>
+          <img src={closeButton} alt="close" onClick={() => navigate(-1)} />
+        </button>
+      </StyledHeaderWrapper>
       <StyledMainWrapper>
         <Messages />
       </StyledMainWrapper>
@@ -30,8 +37,21 @@ const StyledWrapper = styled.div`
   opacity: 0.5;
 `;
 
-const StyledHeader = styled.div`
+const StyledHeaderWrapper = styled.div`
   margin-top: 14.3rem;
+  display: flex;
+  justify-content: center;
+  & button {
+    background: none;
+    border: none;
+  }
+  & img {
+    position: absolute;
+    right: 24.4rem;
+  }
+`;
+
+const StyledHeader = styled.div`
   font-weight: 700;
   font-size: 2.8rem;
   line-height: 160%;
--- src/component/main/Header.jsx
+++ src/component/main/Header.jsx
@@ -60,7 +60,7 @@ const StyledButtonWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
-  z-index: 99;
+  z-index: 1;
   margin-right: 3.9rem;
 
   & button {
