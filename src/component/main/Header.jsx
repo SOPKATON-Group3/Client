b5fcc8ccb8fcddbc13919c1478ea356abfae5f4a
leeseooo <leeseoooo@naver.com>
2022-05-22 06:27:13 +0900

    feat: 헤더 시간 별 배경 중 낮시간대 배경일때 헤더 글씨 색깔 변경

--- src/component/main/Header.jsx
+++ src/component/main/Header.jsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "component/common/assets/icon/logo.svg";
 import UploadMessageModal from "./UploadMessageModal";
 
 function Header() {
   const [showModal, setShowModal] = useState(false);
+  const [isDayTime, setIsDayTime] = useState(false);
+  useEffect(() => {
+    const currentTime = new Date().getHours();
+    if (currentTime >= 8 && currentTime <= 16) {
+      console.log(currentTime);
+      setIsDayTime(true);
+    }
+  }, []);
 
   return (
     <StyledRoot>
@@ -14,17 +22,17 @@ function Header() {
         hideModal={() => setShowModal(false)}
       />
       <StyledLogoWrapper>
-        <Logo />
+        <StyledLogo isDayTime={isDayTime} />
       </StyledLogoWrapper>
-      <StyledButtonWrapper>
+      <StyledButtonWrapper isDayTime={isDayTime}>
         <button onClick={() => setShowModal(true)}>기록하기</button>
-        <Line />
+        <Line isDayTime={isDayTime} />
       </StyledButtonWrapper>
-      <StyledButtonWrapper>
+      <StyledButtonWrapper isDayTime={isDayTime}>
         <Link to="/letters">
           <button>둘러보기</button>
         </Link>
-        <Line />
+        <Line isDayTime={isDayTime} />
       </StyledButtonWrapper>
     </StyledRoot>
   );
@@ -34,6 +42,11 @@ const StyledRoot = styled.div`
   display: flex;
   padding-top: 5.7rem;
   align-items: center;
+`;
+const StyledLogo = styled(Logo)`
+  & > path {
+    fill: ${(props) => props.isDayTime && "#000"};
+  }
 `;
 
 const StyledLogoWrapper = styled.div`
@@ -50,7 +63,7 @@ const StyledButtonWrapper = styled.div`
   z-index: 99;
   margin-right: 3.9rem;
 
-  button {
+  & button {
     width: 8.1rem;
     height: 3.5rem;
     border: none;
@@ -59,13 +72,15 @@ const StyledButtonWrapper = styled.div`
     align-items: center;
     background-color: transparent;
     color: white;
+    color: ${({ isDayTime }) => isDayTime && `#000`};
   }
 `;
 
 const Line = styled.div`
   width: 8.2rem;
   height: 0.2rem;
-  background-color: white;
+  background-color: #fff;
+  background-color: ${({ isDayTime }) => isDayTime && `#000`};
 `;
 
 export default Header;
